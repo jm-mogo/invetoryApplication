c2c961f4590b9220048dcf7d2eb81ba79b58b2d9
mike <josemimore0@gmail.com>
2024-10-18 14:52:50 -0400

    log database data

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
 
+console.log(DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER);
+
 module.exports = {
     PORT,
     DB_HOST,
